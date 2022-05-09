f784740e8bb4cb0baf6c283fb69506865ca09edf
Jock1323 <mirsidiq2002@gmail.com>
2022-05-09 12:02:59 +0500

    asdasdasd

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode:"JIT",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens:{
+        'xs':'320px',
+      },
+      spacing:{
+        'hf':"100vh",
+        'vh': '100vh',
+        'w': '100%',
+        'img1': '500px',
+        'img2': '300px',
+      }
+    },
+  },
+  plugins: [],
+}
